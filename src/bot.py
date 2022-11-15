e294cac597394ca4ff51ee4fc9d3ce222712232b
Akvisvik <anders_kvisvik@hotmail.com>
2022-11-15 16:15:16 +0100

    Recieving info from endpoint, prepping for candle.

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,5 @@
 import websocket
+import json
 
 SOCKET_ENDPOINT = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
@@ -9,7 +10,16 @@ def on_close(ws):
     print('closed connection')
 
 def on_message(ws, message):
-    print('received msg')
+    message_object = json.loads(message)
+    msg_formatted_str = json.dumps(message_object,indent=2)
+    print(msg_formatted_str)
+
+    candle = message_object['k']
+    is_candle_closed = candle['x']
+    close = candle['c']
+
+    if is_candle_closed:
+        print("Candle closed at {}".format(close))
 
 ws = websocket.WebSocketApp(SOCKET_ENDPOINT, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
